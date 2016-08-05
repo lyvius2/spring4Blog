0f7f10279e2986932c0e9d120e34ec20a57d9610
yunshwang <hwang.yunson@joongang.co.kr>
2016-08-05 17:37:22 +0900

    URL / 권한 설정 부분 수정

--- src/main/java/com/yunson/firstapp/TestController.java
+++ src/main/java/com/yunson/firstapp/TestController.java
@@ -1,6 +1,7 @@
 package com.yunson.firstapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
