7b94edd8d5ae1d6b0bd4918a0a720c40e71a8645
yunshwang <hwang.yunson@joongang.co.kr>
2016-08-11 13:23:50 +0900

    connection pool -> Spring에서 생성하는 것으로 변경

---
+++ src/main/java/com/yunson/config/DataSourceConfiguration.java
@@ -0,0 +1,20 @@
+package com.yunson.config;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ * Created by yhwang131 on 2016-08-11.
+ */
+public class DataSourceConfiguration extends DriverManagerDataSource {
+
+	private Cryptography cryptography;
+
+	public void setCryptography(Cryptography cryptography) {
+		this.cryptography = cryptography;
+	}
+
+	@Override
+	public void setUrl(String url) {
+		super.setUrl(cryptography.decrypt(url));
+	}
+}
