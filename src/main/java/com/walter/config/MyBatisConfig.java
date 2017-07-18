501be35a99dbfabdb617b81f6d71d1622316f534
walter.hwang <lyvius2@naver.com>
2017-07-18 20:50:38 +0900

    myBatis JAVA Config 전환 보류 (bean 생성 문제)

--- src/main/java/com/walter/config/MyBatisConfig.java
+++ src/main/java/com/walter/config/MyBatisConfig.java
@@ -1,11 +1,14 @@
 package com.walter.config;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -17,14 +20,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 
 /**
  * myBatis Setting JAVA Config
  * Created by Walter on 2017-07-16.
  */
+/*
 @Configuration
 @PropertySource("classpath:/db.properties")
-@MapperScan(value = "com.walter.dao", sqlSessionFactoryRef = "sqlSessionFactory")
+@MapperScan(basePackages = "com.walter.dao", sqlSessionFactoryRef = "sqlSessionFactory")
 @EnableTransactionManagement
 public class MyBatisConfig {
 
@@ -61,6 +66,8 @@ public class MyBatisConfig {
 		resources[4] = new ClassPathResource("query/PostSQL.xml");
 
 		factoryBean.setMapperLocations(resources);
+		factoryBean.setTypeAliasesPackage("com.walter.model");
+		factoryBean.afterPropertiesSet();
 		return factoryBean.getObject();
 	}
 
@@ -83,3 +90,4 @@ public class MyBatisConfig {
 		return transactionTemplate;
 	}
 }
+*/
